5633486941b24f1eaab7344ced53af3ca85c86ae
Robert <robert.gol.jr@gmail.com>
2017-07-23 01:56:02 -0300

    Camera sample

--- app/src/main/java/com/capivaraprogramadora/hackaton/MainActivity.java
+++ app/src/main/java/com/capivaraprogramadora/hackaton/MainActivity.java
@@ -1,13 +1,21 @@
 package com.capivaraprogramadora.hackaton;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (null == savedInstanceState) {
+            Camera2BasicFragment camera = new Camera2BasicFragment();
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, camera)
+                    .commit();
+
+        }
     }
+
 }
